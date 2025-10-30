be2c80bd108c086f2496dc4bb5cfb7886ded66fe
praveenkumar09 <tpkprav@gmail.com>
2025-10-30 19:34:56 +0800

    Added exception logic for in-sufficient funds and in-sufficient balance

--- src/main/java/com/praveen/customer_service/advice/CustomerServiceExceptionHandler.java
+++ src/main/java/com/praveen/customer_service/advice/CustomerServiceExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.praveen.customer_service.advice;
 
 import com.praveen.customer_service.exception.CustomerNotFoundException;
+import com.praveen.customer_service.exception.InsufficientBalance;
+import com.praveen.customer_service.exception.InsufficientShares;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,6 +23,32 @@ public class CustomerServiceExceptionHandler {
         problemDetail.setType(URI
                 .create("http://example.com/problems/customer-not-found"));
         problemDetail.setTitle("Customer Not Found");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(InsufficientShares.class)
+    public ProblemDetail handleInsufficientSharesException(
+            InsufficientShares ex
+    ){
+        ProblemDetail problemDetail = ProblemDetail
+                .forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                        ex.getMessage());
+        problemDetail.setType(URI
+                .create("http://example.com/problems/insufficient-shares"));
+        problemDetail.setTitle("Insufficient Shares");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(InsufficientBalance.class)
+    public ProblemDetail handleInsufficientBalanceException(
+            InsufficientBalance ex
+    ){
+        ProblemDetail problemDetail = ProblemDetail
+                .forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                        ex.getMessage());
+        problemDetail.setType(URI
+                .create("http://example.com/problems/insufficient-balance"));
+        problemDetail.setTitle("Insufficient Balance");
         return problemDetail;
     }
 }
